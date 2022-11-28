66c27e499f36ec253e66be666b8f9f9b9c99eb66
VuongHuy <96243959+VuongHuy2k1@users.noreply.github.com>
2022-11-28 21:26:55 +0700

    fix_UI

--- user-ui/src/pages/components/ProductList/ProductItem.js
+++ user-ui/src/pages/components/ProductList/ProductItem.js
@@ -9,18 +9,28 @@ import { currencyFormat } from 'src/utils';
 import styles from './ProductList.module.scss';
 const cx = classNames.bind(styles);
 
-function ProductItem({ data, isActive }) {
+function ProductItem({ data, isActive, isShow }) {
   return (
     <>
       <div className={cx('product-item')}>
         <Link to={`/product/${data.gameID}`} className={isActive ? cx('item-wrapper', 'active') : cx('item-wrapper')}>
-          <img className={cx('item-img')} alt="Game" src={imageServices.getImage(data.listImage[0])} />
+          <div className={cx('img')}>
+            <img className={cx('item-img')} alt="Game" src={imageServices.getImage(data.listImage[0])} />
+
+            <div className={cx('prices')}>
+              {data.discount > 0 && <div className={cx('discount')}>-{data.discount}%</div>}
+              {data.discount > 0 && <div className={cx('discount-orginal-price')}>{currencyFormat(data.price)}</div>}
+              <div className={cx('discount-final-price')}>{currencyFormat(data.price * (1 - data.discount / 100))}</div>
+            </div>
+          </div>
+
           <div className={cx('item-detail')}>
             <div className={cx('item-content')}>
               <div className={cx('item-title')}>{data.name}</div>
               <div className={cx('item-category')}>{data.genreName.join(', ')}</div>
             </div>
             <div className={cx('discount-prices')}>
+              {data.discount > 0 && <div className={cx('discount')}>-{data.discount}%</div>}
               {data.discount > 0 && <div className={cx('discount-orginal-price')}>{currencyFormat(data.price)}</div>}
               <div className={cx('discount-final-price')}>{currencyFormat(data.price * (1 - data.discount / 100))}</div>
             </div>
